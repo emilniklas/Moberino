a3c595a675cdabd0955a345134ea7d2deea27ac8
Emil Persson <emil.persson@svt.se>
2019-03-01 10:06:02 +0100

    Initial Commit

---
+++ Moberino/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Moberino
+//
+//  Created by Emil Persson on 2019-03-01.
+//  Copyright © 2019 Emil Persson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
